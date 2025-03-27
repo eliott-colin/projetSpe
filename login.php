034c75405fd9bb446c2b4f7cd568add800686482
eliott-colin <eliott.colin.pro@gmail.com>
2025-03-27 11:54:31 +0100

    First commit + login

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login></title>
+</head>
+<body>
+    <form action="login.php" method="POST" id="form-login">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Mot de passe" required>
+        <button type="submit">Se connecter</button>
+    </form>
+    
+</body>
+</html>
+<?php
+session_start();
+$servername = "localhost"; 
+$username = "root";
+$password = "root"; 
+$dbname = "theatrapp";
+
+try {
+    $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("La connexion a échoué : " . $e->getMessage());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    
+    $stmt = $dbPDO->prepare("SELECT * FROM users WHERE mail = :email");
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && $password === $user['password']) { 
+        $_SESSION['user_id'] = $user['id_user'];  
+        $_SESSION['email'] = $user['mail'];
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['firstname'] = $user['firstname'];
+
+        echo "Connexion réussie ! Redirection...";
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "Email ou mot de passe incorrect.";
+    }
+}
+?>
