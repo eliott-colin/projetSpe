2b2a6dab7ccf7abd69c502f080b77635bd598503
eliott-colin <eliott.colin.pro@gmail.com>
2025-04-03 16:15:12 +0200

    Update style

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -13,9 +13,8 @@ if (!isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de bord</title>
     <link rel="stylesheet" href="../assets/styles/style.css">
+    <link rel="stylesheet" href="../assets/styles/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/styles/style.css">
 </head>
 
 <body>
@@ -26,27 +25,31 @@ if (!isset($_SESSION['user_id'])) {
                 <p class="name"><?php echo $_SESSION['name'] . " " . $_SESSION['firstname']; ?></p>
                 <p class="mail"><?php echo $_SESSION['email']; ?></p>
             </div>
-            <nav>
-                <ul>
-                    <li class="selected">
-                        <a href="dashboard.php"><i class="fa-solid fa-house"></i> Home</a>
-                    </li>
-                    <li>
-                        <a href="workshop.php"><i class="fa-solid fa-chalkboard-teacher"></i> Workshops</a>
-                    </li>
-                    <li>
-                        <a href="show.php"><i class="fa-solid fa-theater-masks"></i> Shows</a>
-                    </li>
-                    <li>
-                        <a href="user_form.php"><i class="fa-solid fa-gear"></i> Settings</a>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href="logout.php">Se déconnecter</a>
-                    </li>
-                </ul>
-            </nav>
+            <div class="container-nav">
+                <nav>
+                    <ul>
+                        <li class="selected">
+                            <a href="dashboard.php"><i class="fa-solid fa-house"></i> Home</a>
+                        </li>
+                        <li>
+                            <a href="workshop.php"><i class="fa-solid fa-chalkboard-teacher"></i> Workshops</a>
+                        </li>
+                        <li>
+                            <a href="show.php"><i class="fa-solid fa-theater-masks"></i> Shows</a>
+                        </li>
+                        <li>
+                            <a href="user_form.php"><i class="fa-solid fa-gear"></i> Settings</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="logout.php">Se déconnecter</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </aside>
     </header>
     <main>
